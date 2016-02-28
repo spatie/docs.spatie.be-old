672cd234e229140d23f689cb902da145c4768349
freek <freek@spatie.be>
2016-02-28 12:59:44 +0100

    add routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,15 +1,30 @@
 <?php
 
-Route::get('/', function() {
-   return redirect('https://spatie.be/opensource');
+Route::get('/', function () {
+    return redirect('https://spatie.be/opensource');
 });
 
-Route::get('laravel-backup', function() {
-    return redirect('laravel-backup/v3/introduction');
+Route::group(['prefix' => 'laravel-backup'], function () {
+
+    Route::get('/', function () {
+        return redirect('laravel-backup/v3/introduction');
+    });
+
+    Route::get('v3', function () {
+        return redirect('laravel-backup/v3/introduction');
+    });
+
 });
 
-Route::get('laravel-medialibrary', function() {
-    return redirect('laravel-medialibrary/v3/introduction');
+Route::group(['prefix' => 'laravel-medialibrary'], function () {
+
+    Route::get('/', function () {
+        return redirect('laravel-medialibrary/v3/introduction');
+    });
+
+    Route::get('v3', function () {
+        return redirect('laravel-medialibrary/v3/introduction');
+    });
 });
 
 Route::get('{slug}', 'PageController@page')->where('slug', '(.*)');
