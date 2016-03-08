34b7d75e75bf0b00100ea833626ab5cc9d170477
freek <freek@spatie.be>
2016-03-08 16:09:55 +0100

    add response cache

---
+++ config/laravel-responsecache.php
@@ -0,0 +1,38 @@
+<?php
+
+return [
+    /**
+     *  This is the master switch to enable of disable the response cache. If set to
+     *  false no responses will be cached.
+     */
+    'enabled' => env('RESPONSE_CACHE_ENABLED', true),
+
+    /**
+     *  The given class will determinate if a request should be cached. The
+     *  default class will cache all successful GET-requests.
+     *
+     *  You can provide your own class given that it implements the
+     *  CacheProfile interface.
+     */
+    'cacheProfile' => Spatie\ResponseCache\CacheProfiles\CacheAllSuccessfulGetRequests::class,
+
+    /**
+     * When using the default CacheRequestFilter this setting controls the
+     * number of minutes responses must be cached.
+     */
+    'cacheLifetimeInMinutes' => 60 * 24 * 7,
+
+    /*
+     * This setting determines if a http header named "Laravel-responsecache"
+     * with the cache time should be added to a cached response. This
+     * can be handy when debugging.
+     */
+    'addCacheTimeHeader' => true,
+
+    /*
+     * Here you may define the cache store that should be used to store
+     * requests. This can be the name of any store that is
+     * configured in app/config/cache.php
+     */
+    'cacheStore' => env('RESPONSE_CACHE_DRIVER', 'file'),
+];
