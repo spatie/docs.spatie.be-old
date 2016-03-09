19ea12d83303777da7bea01082a209196e74ec51
Willem Van Bockstal <willem@spatie.be>
2016-03-09 15:12:39 +0100

    Add target for edit button

--- resources/views/_partials/toolbar.blade.php
+++ resources/views/_partials/toolbar.blade.php
@@ -1,3 +1,3 @@
 <div class="toolbar">
-    <a class="toolbar_item" href="{{ request()->url() }}/edit">Edit on <span>@include('_partials.svg.github')</span></a>
+    <a class="toolbar_item" target="_external" href="{{ request()->url() }}/edit">Edit on <span>@include('_partials.svg.github')</span></a>
 </div>
