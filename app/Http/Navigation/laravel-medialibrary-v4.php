a4b59b9c612ef0df158b2c2004a37c887c179617
freek <freek@spatie.be>
2016-04-05 13:36:34 +0100

    add v4 docs of medialibrary

---
+++ app/Http/Navigation/laravel-medialibrary-v4.php
@@ -0,0 +1,35 @@
+<?php
+
+return [
+    [
+        'Introduction',
+        'Requirements',
+        'Installation & setup',
+        'Questions & issues',
+        'Changelog',
+    ],
+    'Basic usage' => [
+        'Preparing your model',
+        'Associating files',
+        'Retrieving media',
+        'Working with collections',
+    ],
+    'Converting images' => [
+        'Defining conversions',
+        'Retrieving converted images',
+        'Regenerating images',
+    ],
+    'Advanced usage' => [
+        'Working with multiple filesystems',
+        'Adding custom properties',
+        'Generating custom URLs',
+        'Storing media specific manipulations',
+        'Using your own model',
+        'Using a custom directory structure',
+        'Consuming events',
+    ],
+    'API' => [
+        'Adding files',
+        'Defining conversions',
+    ],
+];
---
+++ resources/views/laravel-medialibrary/v4/requirements.md
@@ -0,0 +1,5 @@
+---
+title: Requirements
+---
+
+The Medialibrary package requires **PHP 7.0+** and **Laravel 5.1.0+**. To create derived images **[GD](http://php.net/manual/en/book.image.php)** needs to be installed on your server. If you want to create PDF thumbnails **[Imagick](http://php.net/manual/en/imagick.setresolution.php)** is also required.
