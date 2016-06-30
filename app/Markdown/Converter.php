73c929ee1eef3ae983e331ce5d91b2e1daa2c9ef
Sebastian De Deyne <sebastiandedeyne@gmail.com>
2016-06-30 10:28:58 +0200

    Added ids and links to headers

--- app/Markdown/Converter.php
+++ app/Markdown/Converter.php
@@ -2,6 +2,7 @@
 
 namespace App\Markdown;
 
+use League\CommonMark\Block\Element\Heading;
 use League\CommonMark\DocParser;
 use League\CommonMark\Environment;
 use League\CommonMark\HtmlRenderer;
@@ -11,6 +12,7 @@ class Converter
     public static function convert(string $markdown): string
     {
         $environment = Environment::createCommonMarkEnvironment();
+        $environment->addBlockRenderer(Heading::class, new HeadingRenderer());
 
         $parser = new DocParser($environment);
         $htmlRenderer = new HtmlRenderer($environment);
