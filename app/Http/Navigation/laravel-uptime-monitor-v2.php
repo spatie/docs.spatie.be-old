112cc67f1a89921d09aa9eb97522dad32aa8da95
freek <freek@spatie.be>
2017-01-24 12:47:04 +0100

    wip

---
+++ app/Http/Navigation/laravel-uptime-monitor-v2.php
@@ -0,0 +1,36 @@
+<?php
+
+return [
+    [
+        'Introduction',
+        'Postcardware',
+        'Requirements',
+        'Upgrading from v1',
+        'High level overview',
+        'Installation and setup',
+        'Questions and issues',
+        'Changelog',
+        'About us',
+    ],
+
+    'Monitoring uptime' => [
+        'Getting started',
+        'Events',
+        'Notifications',
+    ],
+
+    'Monitoring ssl certificates' => [
+        'Getting started',
+        'Events',
+        'Notifications',
+    ],
+
+    'Advanced usage' => [
+        'Manually modifying monitors',
+        'Customizing the uptime check',
+        'Customizing notifications',
+        'Disabling monitors',
+        'Using your own model',
+        'Monitoring from multiple locations',
+    ],
+];
