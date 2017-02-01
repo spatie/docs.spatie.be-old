1d96acd92849cf39f4042ae4bc3ac26b1e6bf6e1
Willem Van Bockstal <willem@spatie.be>
2017-02-01 12:02:57 +0100

    Add 404; Fixes #110

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,47 +1,69 @@
 <!DOCTYPE html>
-<html>
+<html data-viewport class="$viewport-loading">
 <head>
-    <title>Not found.</title>
-
-    <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
-
-    <style>
-        html, body {
-            height: 100%;
-        }
-
-        body {
-            margin: 0;
-            padding: 0;
-            width: 100%;
-            color: #B0BEC5;
-            display: table;
-            font-weight: 100;
-            font-family: 'Lato';
-        }
-
-        .container {
-            text-align: center;
-            display: table-cell;
-            vertical-align: middle;
-        }
-
-        .content {
-            text-align: center;
-            display: inline-block;
-        }
-
-        .title {
-            font-size: 72px;
-            margin-bottom: 40px;
-        }
-    </style>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=0, shrink-to-fit=no">
+    <meta name="description" content="Documentation overview for Open Source packages from Spatie.">
+    <title>Open Source Docs | Spatie</title>
+    <link rel="stylesheet" href="https://cloud.typography.com/6194432/608542/css/fonts.css"/>
+    <link rel="stylesheet" href="/build/home.style.css">
+    <script src="/build/home.js" defer></script>
 </head>
 <body>
-<div class="container">
-    <div class="content">
-        <div class="title">Not found</div>
+@include('_partials.analytics')
+<header class="header">
+    <div class="header_background" data-header-background></div>
+    <div class="header_content">
+        <div class="grid">
+            <div class="header_caption">
+                <h1 class="header_caption_title">
+                    <a href="/" >Spatie Docs</a>
+                </h1>
+                <br>
+                <p class="header_caption_slogan">Documentation for our comprehensive packages</p>
+            </div>
+            <div class="header_logos">
+                <a href="https://github.com/spatie" target="_external">
+                    @include('_partials.svg.github')
+                </a>
+                <a href="https://spatie.be" target="_external">
+                    @include('_partials.svg.spatie')
+                </a>
+            </div>
+        </div>
     </div>
-</div>
+</header>
+<main class="main" id="main">
+    <div class="grid">
+        <div class="grid_col -width-2/3">
+            <article class="article">
+                <h1>Docs not found</h1>
+
+                <div class="alert -info">
+                <p>We know nothing about <strong>{{ ltrim($_SERVER['REQUEST_URI'], '/') }}</strong> — or it has yet to be written.<br>
+                    Help us out on <a href="https://github.com/spatie/docs.spatie.be" target="_external">Github</a> if you know more.
+                </p>
+                </div>
+                <hr>
+                <p>What do we know? These are all available documentation sections:</p>
+                @include('home._partials.overview')
+
+                <hr>
+                <p>Find even more <a href="https://spatie.be/en/opensource">Open Source projects</a> on our website.</p>.</p>
+
+            </article>
+        </div>
+    </div>
+</main>
+<footer class="footer">
+    <div class="grid">
+        <div class="footer_content">
+            © {{ date('Y') }} • <a href="https://spatie.be/">Spatie</a>
+            • <a href="https://github.com/spatie">Github</a>
+        </div>
+    </div>
+    <a href="#" data-viewport-scroll class="nav_button -bottom">up</a>
+</footer>
 </body>
 </html>
--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -40,33 +40,7 @@
             <article class="article">
                 <h1>Overview</h1>
 
-                <h2>Laravel</h2>
-
-                <h3><a href="/laravel-activitylog">Laravel-Activitylog Documentation</a></h3>
-                <p>Log the activities of your users.</p>
-
-                <h3><a href="/laravel-backup">Laravel-Backup Documentation</a></h3>
-                <p>Backup your application and database to any filesystem you like.</p>
-
-                <h3><a href="/laravel-medialibrary">Laravel-Medialibrary Documentation</a></h3>
-                <p>Associate files with Eloquent models. Additionally the package can create image manipulations on images and pdfs that have been added in the medialibrary.</p>
-
-                <h3><a href="/laravel-slack-slash-command">Laravel-Slack-Slash-Command Documentation</a></h3>
-                <p>Make a Laravel app respond to a slash command from Slack.</p>
-
-                <h3><a href="/laravel-tags">Laravel-Tags Documentation</a></h3>
-                <p>A powerful tagging package. Batteries included.</p>
-
-                <h3><a href="/laravel-uptime-monitor">Laravel-Uptime-Monitor Documentation</a></h3>
-                <p>A powerful, easy to configure uptime monitor.</p>
-
-                <h2>General PHP</h2>
-
-                <h3><a href="/menu">Menu Documentation</a></h3>
-                <p>Html menu generator.</p>
-
-                <h3><a href="/image">Image Documentation</a></h3>
-                <p>Manipulate images with an expressive API.</p>
+                @include('home._partials.overview')
 
                 <hr>
                 <p>Find even more <a href="https://spatie.be/en/opensource">Open Source projects</a> on our website.</p>
