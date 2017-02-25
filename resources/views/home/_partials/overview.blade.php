2b03d5417ea14a9c39f5e2a905feb4a0cb36a788
freek <freek@spatie.be>
2017-02-25 22:25:03 +0100

    commit

--- resources/views/home/_partials/overview.blade.php
+++ resources/views/home/_partials/overview.blade.php
@@ -9,14 +9,17 @@
 <h3><a href="/laravel-medialibrary">Laravel-Medialibrary Documentation</a></h3>
 <p>Associate files with Eloquent models. Additionally the package can create image manipulations on images and pdfs that have been added in the medialibrary.</p>
 
-<h3><a href="/laravel-slack-slash-command">Laravel-Slack-Slash-Command Documentation</a></h3>
-<p>Make a Laravel app respond to a slash command from Slack.</p>
+<h3><a href="/laravel-uptime-monitor">Laravel-Uptime-Monitor Documentation</a></h3>
+<p>A powerful, easy to configure uptime monitor.</p>
+
+<h3><a href="/laravel-server-monitor">Laravel-Server-Monitor Documentation</a></h3>
+<p>Monitor the health of your servers.</p>
 
 <h3><a href="/laravel-tags">Laravel-Tags Documentation</a></h3>
 <p>A powerful tagging package. Batteries included.</p>
 
-<h3><a href="/laravel-uptime-monitor">Laravel-Uptime-Monitor Documentation</a></h3>
-<p>A powerful, easy to configure uptime monitor.</p>
+<h3><a href="/laravel-slack-slash-command">Laravel-Slack-Slash-Command Documentation</a></h3>
+<p>Make a Laravel app respond to a slash command from Slack.</p>
 
 <h2>General PHP</h2>
 
